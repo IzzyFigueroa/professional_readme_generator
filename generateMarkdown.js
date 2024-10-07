3fc17543d66fda9ab3410d0483c06f88898314d0
Izzy Figeuroa <izzyfigueroa6@gmail.com>
2024-10-07 10:01:48 -0400

    Still working to get this working

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -41,6 +41,59 @@ function renderLicenseSection(license) {
 
 // TODO: Create a function to generate markdown for README
 async function generateMarkdown() {
+  const questions = [
+    {
+        message: 'Please enter the title of your README?',
+        name: 'title'
+    },
+    {
+        message: 'Please enter the description of your README.',
+        name: 'description'
+    },
+    {
+        message: 'Please provide the installation instructions',
+        name: 'installInstructions'
+    },
+    {
+        message: 'What is the usage of this file?',
+        name: 'usage'
+    },
+    {
+        message: 'What are the contribution guidelines?',
+        name: 'contribution'
+    },
+    {
+        message: 'Are there any test instructions?',
+        name: 'testing'
+    },
+    {
+        message: 'Please choose a license below.',
+        name: 'license',
+        type: 'list',
+        choices: [
+            {
+                name: 'MIT License',
+                value: '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+            },
+            {
+                name: 'Apache 2.0 License',
+                value: '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+            },
+            {
+                name: 'GNU GPL v3 License',
+                value: '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
+            },
+            {
+                name: 'BSD 3-Clause License',
+                value: '[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)'
+            },
+            {
+                name: 'Mozilla Public License 2.0',
+                value: '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)'
+            }
+        ]
+    }
+];
   const answers = await inquirer.prompt(questions);
   return `# ${answers.title}
   # Description\n ${answers.description}
@@ -50,7 +103,6 @@ async function generateMarkdown() {
   # Testing\n ${answers.testing}
   ${renderLicenseSection(answers.license)}
 `;
-
 
 }
 
