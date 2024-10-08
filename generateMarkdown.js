ad0c0cdf8bbfb10c6c723b5bd4aa7c5ef4bf65e0
Izzy Figeuroa <izzyfigueroa6@gmail.com>
2024-10-07 22:39:26 -0400

    almost done with HW

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,14 +1,23 @@
+import fs from 'fs';
 import inquirer from "inquirer";
-
-// import {promsises as fs} from 'fs
+import { questions, writeToFile } from './index.js';
 
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
+  const badges = {
+    'MIT': '![MIT License](https://img.shields.io/badge/License-MIT-yellow.svg)',
+    'GPLv3': '![GPLv3 License](https://img.shields.io/badge/License-GPLv3-blue.svg)',
+    'Apache 2.0': '![Apache 2.0 License](https://img.shields.io/badge/License-Apache%202.0-green.svg)',
+    'BSD 3-Clause': '![BSD 3-Clause License](https://img.shields.io/badge/License-BSD%203--Clause-orange.svg)'
+  };
+
   if (!license) {
     return '';
+  } else {
+    return badges[license];
   }
-  return license;
+
 }
 
 // TODO: Create a function that returns the license link
@@ -18,11 +27,11 @@ function renderLicenseLink(license) {
     return '';
   }
   const licenseLinks = {
-    'MIT License': 'https://opensource.org/licenses/MIT',
-    'Apache 2.0 License': 'https://opensource.org/licenses/Apache-2.0',
-    'GNU GPL v3 License': 'https://www.gnu.org/licenses/gpl-3.0',
-    'BSD 3-Clause License': 'https://opensource.org/licenses/BSD-3-Clause',
-    'Mozilla Public License 2.0': 'https://opensource.org/licenses/MPL-2.0'
+    'MIT': 'https://opensource.org/licenses/MIT',
+    'Apache 2.0': 'https://opensource.org/licenses/Apache-2.0',
+    'GPLv3': 'https://www.gnu.org/licenses/gpl-3.0',
+    'BSD 3-Clause': 'https://opensource.org/licenses/BSD-3-Clause'
+
   };
   return licenseLinks[license];
 }
@@ -34,75 +43,42 @@ function renderLicenseSection(license) {
   if (!license) {
     return '';
   }
-  return `## License\nThis project is licensed under the terms of the ${license}. For more information, please visit [this link]($
-  {renderLicenseLink(license)}).\n`;
+  return `## License\nThis project is licensed under the terms of the ${license}. For more information, please visit ${renderLicenseLink(license)}.\n`;
 }
 
 
 // TODO: Create a function to generate markdown for README
 async function generateMarkdown() {
-  const questions = [
-    {
-        message: 'Please enter the title of your README?',
-        name: 'title'
-    },
-    {
-        message: 'Please enter the description of your README.',
-        name: 'description'
-    },
-    {
-        message: 'Please provide the installation instructions',
-        name: 'installInstructions'
-    },
-    {
-        message: 'What is the usage of this file?',
-        name: 'usage'
-    },
-    {
-        message: 'What are the contribution guidelines?',
-        name: 'contribution'
-    },
-    {
-        message: 'Are there any test instructions?',
-        name: 'testing'
-    },
-    {
-        message: 'Please choose a license below.',
-        name: 'license',
-        type: 'list',
-        choices: [
-            {
-                name: 'MIT License',
-                value: '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
-            },
-            {
-                name: 'Apache 2.0 License',
-                value: '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
-            },
-            {
-                name: 'GNU GPL v3 License',
-                value: '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
-            },
-            {
-                name: 'BSD 3-Clause License',
-                value: '[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)'
-            },
-            {
-                name: 'Mozilla Public License 2.0',
-                value: '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)'
-            }
-        ]
-    }
-];
+
   const answers = await inquirer.prompt(questions);
-  return `# ${answers.title}
-  # Description\n ${answers.description}
-  # Installation\n ${answers.installInstructions}
-  # Usage\n ${answers.usage}
-  # Contribution Guidelines\n ${answers.contribution}
-  # Testing\n ${answers.testing}
+  const content = `
+  # ${answers.title}
+  # ${renderLicenseBadge(answers.license)}
+
+  ## Table of Contents
+  - [Questions](#questions)
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution Guidelines](#contribution-guidelines)
+  - [Testing](#testing)
+  - [License](#license)
+
+  ## Questions\n My Github username is https://github.com/${answers.github}
+  \n Please contact me at ${answers.email}
+
+  ## Description\n ${answers.description}
+
+  ## Installation\n ${answers.installInstructions}
+  ## Usage\n ${answers.usage}
+
+  ## Contribution Guidelines\n ${answers.contribution}
+  ## Testing\n ${answers.testing}
+
   ${renderLicenseSection(answers.license)}
+
 `;
+  writeToFile('README.md', content);
 
 }
 
--- index.js
+++ index.js
@@ -1,67 +1,71 @@
 // TODO: Include packages needed for this application
-import inquirer from 'inquirer';
 import fs from 'fs';
+import inquirer from 'inquirer';
 import generateMarkdown from './generateMarkdown.js';
 import figlet from 'figlet';
 // import { error } from 'console';
 // TODO: Create an array of questions for user input
-// const questions = [
-//     {
-//         message: 'Please enter the title of your README?',
-//         name: 'title'
-//     },
-//     {
-//         message: 'Please enter the description of your README.',
-//         name: 'description'
-//     },
-//     {
-//         message: 'Please provide the installation instructions',
-//         name: 'installInstructions'
-//     },
-//     {
-//         message: 'What is the usage of this file?',
-//         name: 'usage'
-//     },
-//     {
-//         message: 'What are the contribution guidelines?',
-//         name: 'contribution'
-//     },
-//     {
-//         message: 'Are there any test instructions?',
-//         name: 'testing'
-//     },
-//     {
-//         message: 'Please choose a license below.',
-//         name: 'license',
-//         type: 'list',
-//         choices: [
-//             {
-//                 name: 'MIT License',
-//                 value: '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
-//             },
-//             {
-//                 name: 'Apache 2.0 License',
-//                 value: '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
-//             },
-//             {
-//                 name: 'GNU GPL v3 License',
-//                 value: '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
-//             },
-//             {
-//                 name: 'BSD 3-Clause License',
-//                 value: '[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)'
-//             },
-//             {
-//                 name: 'Mozilla Public License 2.0',
-//                 value: '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)'
-//             }
-//         ]
-//     }
-// ];
+export const questions = [
+    {
+        message: 'Please enter the title of your README?',
+        name: 'title'
+    },
+    {
+        message: 'What is your Github username?',
+        name: 'github'
+    },
+    {
+        message: 'Please provide your email address',
+        name: 'email'
+    },
+    {
+        message: 'Please enter the description of your README.',
+        name: 'description'
+    },
+    {
+        message: 'Please provide the installation instructions',
+        name: 'installInstructions'
+    },
+    {
+        message: 'What is the usage of this file?',
+        name: 'usage'
+    },
+    {
+        message: 'What are the contribution guidelines?',
+        name: 'contribution'
+    },
+    {
+        message: 'Are there any test instructions?',
+        name: 'testing'
+    },
+    {
+        message: 'Please choose a license below.',
+        name: 'license',
+        type: 'list',
+        choices: [
+            {
+                name: 'MIT License',
+                value: 'MIT'
+            },
+            {
+                name: 'Apache 2.0 License',
+                value: 'Apache 2.0'
+            },
+            {
+                name: 'GNU GPL v3 License',
+                value: 'GPLv3'
+            },
+            {
+                name: 'BSD 3-Clause License',
+                value: 'BSD 3-Clause'
+            }
+        ]
+    }
+];
 
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
+export function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, (error) => {
         if (error) {
             console.log('Uh oh! Something went wrong', error);
@@ -83,10 +87,13 @@ async function init() {
     switch (menuObj.menuChoice) {
         case 'Create a Markdown File':
             generateMarkdown();
-            writeToFile('README.md', questions);
             break;
         default:
             figlet('Thanks for visiting!', (err, rendered) => {
+                if (err) {
+                    console.error('Error', err);
+                    return;
+                }
                 console.log(rendered)
             }
 
@@ -96,3 +103,4 @@ async function init() {
 
 // Function call to initialize app
 init();
+
